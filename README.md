c5bbff86e17a50b0122d77bbb59f935db8d8d673
eun0901 <jk2310400@ajou.ac.kr>
2022-09-11 16:01:12 +0900

    9/11 second commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 npm install styled-components prop-types
+
+npm install react-native-html-parser
--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import React, { useState } from 'react';
 import {View, StyleSheet, Text} from 'react-native';
 import SearchBox from './components/SearchBox';
+import { DOMParser } from 'xmldom';
 
 const App = () => {
     const [word, setWord] = useState('');
-    const [station, setStation] = useState('');
-    const primekey = 'UkgvlYP2LDE6M%2Blz55Fb0XVdmswp%2Fh8uAUZEzUbby3OYNo80KGGV1wtqyFG5IY0uwwF0LtSDR%2FIwPGVRJCnPyw%3D%3D';
+    const [station, setStation] = useState({});
 
     const _handleTextChange = text => {
         setWord(text);
     };
-    const _addStation = () => {
-        fetch(`http://apis.data.go.kr/6410000/busstationservice/getBusStationList?serviceKey=${primekey}&keyword=${word}`)
-        .then(r => r.json()).then(r => {
-            console.log(r);
-            setStation(r.results);
-            setWord('');
-        }).catch(e => console.log('error when search station'));
-    };
+
+    const _addStation = async(e)=>{
+          try{
+            var xhr = new XMLHttpRequest();
+            const API_KEY = 'UkgvlYP2LDE6M%2Blz55Fb0XVdmswp%2Fh8uAUZEzUbby3OYNo80KGGV1wtqyFG5IY0uwwF0LtSDR%2FIwPGVRJCnPyw%3D%3D';
+            const url = 'http://apis.data.go.kr/6410000/busstationservice/getBusStationList'; /*URL*/
+            const result = encodeURIComponent(word);
+            var queryParams = `${url}?serviceKey=${API_KEY}&keyword=${word}`;
+            xhr.open('GET', queryParams);
+            xhr.onreadystatechange = function () {
+              if (this.readyState == 4) {
+                console.log(this.responseText);
+                var xmlDoc = new DOMParser().parseFromString(this.responseText, 'text/xml');
+                console.log("success?");
+                let value = xmlDoc.getElementsByTagName("stationName")[0].textContent;
+                console.log(value);
+                setStation(value);
+              }
+            }
+          xhr.send('');
+          }
+          catch(err){
+            alert(err);
+          }
+      };
 
     const _onBlur = () => {
         setWord('');
@@ -34,9 +51,11 @@ const App = () => {
             onChangeText={_handleTextChange}
             onSubmitEditing={_addStation}
             onBlur={_onBlur}
-            />    
-            <Text style={styles.title}>{word}</Text>
-            <Text style={styles.title}>{station}</Text>
+            />
+            {
+              Object.keys(station).length !== 0 && (
+                <Text className="stationName">{station}</Text>
+              )}
         </View>
     );
 };
